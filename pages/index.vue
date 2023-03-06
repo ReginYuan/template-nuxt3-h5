0e0b49b421bfa76bbd5ce88e506db246263776a8
reginyuan <2496943860@qq.com>
2023-03-06 08:52:08 +0800

    修改对应路由

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 // 获取组件的实例
+
+onMounted(() => {
+   function middleware({ store, route, redirect, params, query, req, res }) {
+      redirect('/home') // 默认跳转页面的路由
+   }
+})
 </script>
 
 <template>
