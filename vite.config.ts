376c6ccddbd078e8c5dee9e0077bf0d271f3de69
reginyuan <2496943860@qq.com>
2023-04-23 15:47:34 +0800

    feat: 修改项目结构

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,12 @@
-// vite.config.ts
-import {
-  createStyleImportPlugin,
-  VantResolve
-} from 'vite-plugin-style-import'
- 
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
+
 export default {
   plugins: [
-    createStyleImportPlugin({
-      resolves: [VantResolve()],
-      libs: [
-        {
-          libraryName: 'vant',
-          esModule: true,
-          resolveStyle: (name: string) => {
-            if (name == 'show-toast') {
-              return `../es/toast/style/index`; //修改vant引入路径
-            } else {
-              return `../es/${name}/style/index`; //修改vant引入路径
-            }
-          }
-        }
-      ]
-    })
-  ]
+    vue(),
+    Components({
+      resolvers: [VantResolver()],
+    }),
+  ],
 };
